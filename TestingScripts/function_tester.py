be8286a14fc4df579c8d5fc85cc1c0cec8c76b53
ericlyall <ericlyall1428@gmail.com>
2021-04-09 11:40:06 -0700

    Commit from Eric's laptop

--- TestingScripts/function_tester.py
+++ TestingScripts/function_tester.py
@@ -3,11 +3,5 @@ import pandas as pd
 array1 = np.array([3,4,5,6,7,8,9,10])
 array2 = ([2,5,1,3,5,7,4,12])
 
-df = pd.DataFrame()
 
-df['Array1'] = array1
-df['Array2'] = array2
-
-df.to_csv(r'C:\Users\erlyall\PycharmProjects\dpu\CarolinaTestData.csv', index = False)
-
-print('Complete')
+## Checking if this shows up on a commit from Eric's laptop on April 9th, 2021.
